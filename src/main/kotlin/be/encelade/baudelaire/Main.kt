313c55ff3d46341b48ec5c0d8c2f5bf2ac4bfedb
benckx <benoit.vleminckx@restore.eu>
2021-08-13 16:25:17 +0200

    format QR codes

--- src/main/kotlin/be/encelade/baudelaire/Main.kt
+++ src/main/kotlin/be/encelade/baudelaire/Main.kt
@@ -1,8 +1,11 @@
 package be.encelade.baudelaire
 
 import com.google.zxing.BarcodeFormat.QR_CODE
+import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+import com.google.zxing.qrcode.decoder.Version
 import org.apache.commons.io.FileUtils
 import org.jsoup.Jsoup
 import java.io.File
@@ -10,10 +13,19 @@ import java.net.URL
 import java.nio.file.Path
 
 fun main() {
+    FileUtils.deleteDirectory(File("img/"))
+    File("img").mkdir()
+
     val charSetName = "UTF-8"
-    val qrSize = 400
-    val limit = 900
+    val size = 500
+    val limit = 1370
     val readMeLines = mutableListOf<String>()
+    val textLengths = mutableListOf<Int>()
+    val hints = mutableMapOf<EncodeHintType, Any>()
+    hints[EncodeHintType.ERROR_CORRECTION] = ErrorCorrectionLevel.M
+    hints[EncodeHintType.QR_VERSION] = Version.getVersionForNumber(30)!!
+    hints[EncodeHintType.MARGIN] = 1
+//    hints[EncodeHintType.CHARACTER_SET] = charSetName
 
     (1..134)
             .forEach { i ->
@@ -28,10 +40,11 @@ fun main() {
 
                     val title = titles.first()!!.text()
                     val textAndTitle = title + "\n\n" + text
+                    textLengths += text.length
 
-                    if (textAndTitle.length <= limit) {
-                        val matrix = MultiFormatWriter().encode(textAndTitle, QR_CODE, qrSize, qrSize)
+                    if (textAndTitle.length < limit) {
                         val imagePath = "img/qr$i.png"
+                        val matrix = MultiFormatWriter().encode(textAndTitle, QR_CODE, size, size, hints)
                         MatrixToImageWriter.writeToPath(matrix, "png", Path.of(imagePath))
 
                         readMeLines += "### $title"
@@ -46,6 +59,8 @@ fun main() {
     val header = FileUtils.readFileToString(getResourceAsFile("/header.md"), charSetName)
     val content = header + readMeLines.joinToString("\n")
     FileUtils.writeStringToFile(readMeFile, content, charSetName)
+
+    println("text length (min/avg/max): ${textLengths.minOrNull()} / ${textLengths.average().toInt()} / ${textLengths.maxOrNull()}")
 }
 
 private fun getResourceAsFile(path: String): File {
